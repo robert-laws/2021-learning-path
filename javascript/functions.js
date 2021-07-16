b0e8989b3302e793f5b1fa6b7f76a16d6da0f885
Robert Laws <roblaws@me.com>
2021-07-16 18:43:39 -0400

    update function examples

--- javascript/functions.js
+++ javascript/functions.js
@@ -80,3 +80,15 @@ console.log(greetings('melvin'));
 function greetings(name) {
   return `Howdy, ${name}!`;
 }
+
+function add(a, b) {
+  return a + b;
+}
+
+const add = (a, b) => {
+  a + b;
+};
+
+const add = function (a, b) {
+  return a + b;
+};
